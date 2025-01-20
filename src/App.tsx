bc794ab940b771b9cba0c628b028eb251e6142d7
Minh Dao <daominh91197@gmail.com>
2025-01-21 00:01:15 +0200

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden"></main>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+import { resolve } from "node:path";
+import cssplugin from "./cssplugin";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "circular-web": ["circular-web", "sans-serif"],
+        "roobert-medium": ["roobert-medium", "sans-serif"],
+        "roobert-regular": ["roobert-regular", "sans-serif"],
+      },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#DFDFF2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8e983f",
+          300: "#edff66",
+        },
+      },
+    },
+  },
+  plugins: [cssplugin(resolve(__dirname, "./src/index.css"))],
+};
